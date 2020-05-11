09f552dff0363980591a9d6348b5c90177bf8ba0
CropPredication <croppred@gmail.com>
2020-05-12 00:03:59 +0530

    New commit

---
+++ app/src/main/java/com/tutlane/cropprediction/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tutlane.cropprediction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+public class MainActivity extends AppCompatActivity {
+    private Spinner spinner;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
